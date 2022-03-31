dc7d6118e7028f173c4daeed44ef596395167f5a
gusgluna <gusgluna@gmail.com>
2022-03-31 12:38:35 -0600

    [FIXING]  keyboard responsive

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -1,19 +1,25 @@
 import React, { useContext, useEffect } from "react";
 import { AppContext } from "../App";
-import "../styles/Letter.css"
-const classNames = require('classnames');
+import "../styles/Letter.css";
+const classNames = require("classnames");
 
 function Letter({ letterPos, attemptVal }) {
-  const { board, setDisabledLetters, currAttempt, correctWord, setAlmostLetters, setCorrectLetters } =
-    useContext(AppContext);
+  const {
+    board,
+    setDisabledLetters,
+    currAttempt,
+    correctWord,
+    setAlmostLetters,
+    setCorrectLetters,
+  } = useContext(AppContext);
   const letter = board[attemptVal][letterPos];
   const correct = correctWord.toUpperCase()[letterPos] === letter;
   const almost =
-  !correct && letter !== "" && correctWord.toUpperCase().includes(letter);
+    !correct && letter !== "" && correctWord.toUpperCase().includes(letter);
 
   const letterState =
-  currAttempt.attempt > attemptVal &&
-  (correct ? "correct" : almost ? "almost" : "error");
+    currAttempt.attempt > attemptVal &&
+    (correct ? "correct" : almost ? "almost" : "error");
 
   let letterClass = classNames({
     letter: true,
@@ -21,19 +27,29 @@ function Letter({ letterPos, attemptVal }) {
     typedLetter: letter,
     correct: letterState === "correct",
     almost: letterState === "almost",
-    error: letterState === "error"
-  })
+    error: letterState === "error",
+  });
 
   useEffect(() => {
-    if(currAttempt.attempt > attemptVal){
-    if (letter !== "" && !correct && !almost) {
-      setDisabledLetters((prev) => [...prev, letter]);
-    } else if (letter !== "" && almost && !correct){
-      setAlmostLetters((prev)=>[...prev, letter])
-    } else if(letter !== "" && !almost && correct){
-      setCorrectLetters((prev)=>[...prev, letter])
-    }}
-  }, [currAttempt.attempt, setAlmostLetters, setCorrectLetters, setDisabledLetters, almost, correct, letter, attemptVal]);
+    if (currAttempt.attempt > attemptVal) {
+      if (letter !== "" && !correct && !almost) {
+        setDisabledLetters((prev) => [...prev, letter]);
+      } else if (letter !== "" && almost && !correct) {
+        setAlmostLetters((prev) => [...prev, letter]);
+      } else if (letter !== "" && !almost && correct) {
+        setCorrectLetters((prev) => [...prev, letter]);
+      }
+    }
+  }, [
+    currAttempt.attempt,
+    setAlmostLetters,
+    setCorrectLetters,
+    setDisabledLetters,
+    almost,
+    correct,
+    letter,
+    attemptVal,
+  ]);
   return (
     <div className={letterClass} id={`${letterPos}${attemptVal}`}>
       {letter}
